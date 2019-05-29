e2b473a318b7705f0b010c8d589538a863ceeee0
Kevin A. Brown <kevin.brown.jm@gmail.com>
2019-05-29 02:30:05 -0500

    Nikola auto commit.
    Nikola version: 8.0.1

--- cache/.mako.tmp/list_post.tmpl.py
+++ cache/.mako.tmp/list_post.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1552806173.1078176
+_modified_time = 1559115005.167162
 _enable_loop = True
 _template_filename = '/usr/local/lib/python3.6/dist-packages/nikola/data/themes/base/templates/list_post.tmpl'
 _template_uri = 'list_post.tmpl'
@@ -36,17 +36,17 @@ def render_body(context,**pageargs):
         _import_ns = {}
         _mako_get_namespace(context, 'archive_nav')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
+        kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
+        date_format = _import_ns.get('date_format', context.get('date_format', UNDEFINED))
+        title = _import_ns.get('title', context.get('title', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
         def extra_head():
             return render_extra_head(context._locals(__M_locals))
-        kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
-        title = _import_ns.get('title', context.get('title', UNDEFINED))
         feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         archive_nav = _mako_get_namespace(context, 'archive_nav')
+        posts = _import_ns.get('posts', context.get('posts', UNDEFINED))
         def content():
             return render_content(context._locals(__M_locals))
-        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
-        date_format = _import_ns.get('date_format', context.get('date_format', UNDEFINED))
-        posts = _import_ns.get('posts', context.get('posts', UNDEFINED))
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
@@ -92,14 +92,14 @@ def render_content(context,**pageargs):
         _mako_get_namespace(context, 'archive_nav')._populate(_import_ns, ['*'])
         _mako_get_namespace(context, 'feeds_translations')._populate(_import_ns, ['*'])
         kind = _import_ns.get('kind', context.get('kind', UNDEFINED))
+        date_format = _import_ns.get('date_format', context.get('date_format', UNDEFINED))
         title = _import_ns.get('title', context.get('title', UNDEFINED))
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
         feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         archive_nav = _mako_get_namespace(context, 'archive_nav')
+        posts = _import_ns.get('posts', context.get('posts', UNDEFINED))
         def content():
             return render_content(context)
-        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
-        date_format = _import_ns.get('date_format', context.get('date_format', UNDEFINED))
-        posts = _import_ns.get('posts', context.get('posts', UNDEFINED))
         __M_writer = context.writer()
         __M_writer('\n<article class="listpage">\n    <header>\n        <h1>')
         __M_writer(filters.html_escape(str(title)))
--- output/publications/index.rst
+++ output/publications/index.rst
@@ -7,5 +7,24 @@
 .. description: 
 .. type: text
 
-.. publication_list:: files/brown.bib
+
+Refereed Publications
+---------------------
+
+.. publication_list:: files/conf_workshop.bib files/posters.bib 
+        :highlight_author: Kevin A. Brown; K. Brown; K. A. Brown; Kevin Brown
+
+
+Unrefereed Publications
+-----------------------
+
+.. publication_list:: files/unrefereed.bib 
+        :highlight_author: Kevin A. Brown; K. Brown; K. A. Brown; Kevin Brown
+
+
+Invited Talks and Other Presentations
+-------------------------------------
+
+
+.. publication_list:: files/invited.bib 
         :highlight_author: Kevin A. Brown; K. Brown; K. A. Brown; Kevin Brown
